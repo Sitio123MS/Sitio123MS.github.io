90d7f05186e9b20e2f5ca8ab174d42a610942e9b
MiniSuper <minisupereltriangulo@gmail.com>
2022-08-21 20:02:19 -0600

    SitioWeb

---
+++ contacto1.php
@@ -0,0 +1,26 @@
+@charset "utf-8";
+/* CSS Document */
+.container{
+margin-top: 50px;
+}
+.carousel-caption{
+color:#1AD5B5;
+background-color:rgba(116,114,114,0.77);
+}
+.carousel{
+	padding: 30px;
+	height-top: 30px;
+}
+.pie{
+  background-color: rgba(241,151,4,0.72);
+	margin-bottom:20px;
+}
+div.col-md-1{
+	margin-bottom:2px;
+	top: 10px;
+}
+
+.pie1{
+	background-color: rgba(47,230,199,0.67);
+}
+
